74579e6ce826ff520d6edb3e8c7023b84d051845
D-Faison <dfaison2017@gmail.com>
2025-05-02 18:15:44 -0400

    Creation

---
+++ module-10/FaisonMod10.js
@@ -0,0 +1,16 @@
+
+function turnBlue(){
+    document.getElementById('lilBuddy').src='images/MushroomThingBlue.png';
+}
+
+function turnNormal(){
+    document.getElementById('lilBuddy').src='images/MushroomThingNormal.png';
+}
+
+function sayHi(){
+    document.getElementById('lilBuddy').src='images/MushroomThingWave.png';
+}
+
+function turnGreen(){
+    document.getElementById('lilBuddy').src='images/MushroomThingGreen.png';
+}
